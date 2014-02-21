ae1f627c9ede24b3096ae0ebdd23538ce0dfa09e
Armin Balalaie <armin.balalaie@gmail.com>
2014-02-21 17:10:50 +0330

    GraphBuilder has been changed to work with configuration.

--- src/graph_builder/GraphBuilder.java
+++ src/graph_builder/GraphBuilder.java
@@ -1,14 +1,26 @@
 package graph_builder;
 
-import java.sql.Date;
 import java.util.ArrayList;
+
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+
+import config.Configurer;
 
 public abstract class GraphBuilder {
 	Graph graph;
+	Configurer config;
+	SVNClientManager svnClient;
 	
 	public GraphBuilder()
 	{
 		graph = new Graph();
+		this.config = null;
+	}
+	
+	public GraphBuilder(Configurer config)
+	{
+		graph = new Graph();
+		this.config = config;
 	}
 	
 	protected abstract ArrayList<ChangeItem> extractItem(int revisionNumber);
@@ -16,12 +28,5 @@ public abstract class GraphBuilder {
 	public void makeGraph(int revisionFrom, int revisionTo)
 	{
 		// the graph creation code that uses extractItem.
-	}
-	
-	public void makeGraph(Date from, Date to)
-	{
-		// the graph creation code that uses extractItem.
-	}
-	
-	
+	}	
 }
