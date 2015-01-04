d69930f341de44084b97c1e3120fe64e8e969c4f
Mohammad <mabbasi90.class@gmail.com>
2015-01-05 00:52:51 +0330

    basic new graph utilities added

--- src/ConfigFactory.java
+++ src/ConfigFactory.java
@@ -1,54 +1,48 @@
+import config.IConfigurer;
+import config.JHotDrawConfigurerSample;
+import config.TomCatConfigurerSample;
 import graph_builder.FileGraphBuilder;
 import graph_builder.GraphBuilder;
 import graph_builder.MethodGraphBuilder;
 
 import java.util.HashMap;
 
-import config.IConfigurer;
-import config.JHotDrawConfigurer;
-import config.TomCatConfigurer;
-
 
 public class ConfigFactory {
-	private HashMap<String, IConfigurer> configInstances = new HashMap<String, IConfigurer>();
-	private HashMap<String, GraphBuilder> builderInstances = new HashMap<String, GraphBuilder>();
-	
-	public ConfigFactory()
-	{
-		configInstances.put("jhotdraw", new JHotDrawConfigurer());
-		configInstances.put("tomcat", new TomCatConfigurer());
-		
-		builderInstances.put("file", new FileGraphBuilder());
-		builderInstances.put("method", new MethodGraphBuilder());
-	}
-	
-	public String getConfigurersList()
-	{
-		String ret = "";
-		for(String key : configInstances.keySet())
-			ret += "|" + key;
-		return ret.substring(1);
-	}
-	
-	public String getBuildersList()
-	{
-		String ret = "";
-		for(String key : builderInstances.keySet())
-			ret += "|" + key;
-		return ret.substring(1);
-	}
-	
-	public IConfigurer getConfigurer(String key)
-	{
-		if(configInstances.containsKey(key))
-			return configInstances.get(key);
-		return null;
-	}
-	
-	public GraphBuilder getBuilder(String key)
-	{
-		if(builderInstances.containsKey(key))
-			return builderInstances.get(key);
-		return null;
-	}
+    private HashMap<String, IConfigurer> configInstances = new HashMap<String, IConfigurer>();
+    private HashMap<String, GraphBuilder> builderInstances = new HashMap<String, GraphBuilder>();
+
+    public ConfigFactory() {
+        configInstances.put("jhotdraw", new JHotDrawConfigurerSample());
+        configInstances.put("tomcat", new TomCatConfigurerSample());
+
+        builderInstances.put("file", new FileGraphBuilder());
+        builderInstances.put("method", new MethodGraphBuilder());
+    }
+
+    public String getConfigurersList() {
+        String ret = "";
+        for (String key : configInstances.keySet())
+            ret += "|" + key;
+        return ret.substring(1);
+    }
+
+    public String getBuildersList() {
+        String ret = "";
+        for (String key : builderInstances.keySet())
+            ret += "|" + key;
+        return ret.substring(1);
+    }
+
+    public IConfigurer getConfigurer(String key) {
+        if (configInstances.containsKey(key))
+            return configInstances.get(key);
+        return null;
+    }
+
+    public GraphBuilder getBuilder(String key) {
+        if (builderInstances.containsKey(key))
+            return builderInstances.get(key);
+        return null;
+    }
 }
