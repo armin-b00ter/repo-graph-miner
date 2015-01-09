13c9d13e1f6e342ebe49bedb67f81bbab7902043
Mohammad <mabbasi90.class@gmail.com>
2015-01-09 22:16:17 +0330

    using min weights remover filter

--- src/graph_builder/newgraph/IslandsAlgorithmExecuter.java
+++ src/graph_builder/newgraph/IslandsAlgorithmExecuter.java
@@ -3,6 +3,7 @@ package graph_builder.newgraph;
 import graph_builder.newgraph.filter.FiltersApplier;
 import graph_builder.newgraph.filter.FirstDecilesRemoveFilter;
 import graph_builder.newgraph.filter.MinNodeFilter;
+import graph_builder.newgraph.filter.MinWeightsRemoveFilter;
 
 import java.io.*;
 import java.util.HashMap;
@@ -14,6 +15,7 @@ public class IslandsAlgorithmExecuter {
 
     public static final String MIN_NODES = "minNodes";
     public static final String FIRST_DECILES_NUM = "firstDecilesNum";
+    public static final String MIN_WEIGHTS_NUM = "minWeightsNum";
 
     public HashMap<String, String> getParams() {
         return params;
@@ -27,8 +29,10 @@ public class IslandsAlgorithmExecuter {
     public IslandsAlgorithmExecuter(String outputDir, String inputDir) {
         this.outputDir = outputDir;
         this.inputFile = inputDir;
-        params.put(MIN_NODES, "5");
-        params.put(FIRST_DECILES_NUM, "1.0");
+        params.put(MIN_NODES, "2");
+//        params.put(FIRST_DECILES_NUM, "1.0");
+        params.put(MIN_WEIGHTS_NUM, "1");
+
     }
 
     public void execute() throws IOException {
@@ -38,9 +42,11 @@ public class IslandsAlgorithmExecuter {
             outputFile += "_" + key + "-" + params.get(key);
         }
         NewGraph newGraph = new NewGraph(inputFile);
-        FirstDecilesRemoveFilter firstDecileRemoveFilter = new FirstDecilesRemoveFilter(Double.valueOf(params.get(FIRST_DECILES_NUM)));
+//        FirstDecilesRemoveFilter firstDecileRemoveFilter = new FirstDecilesRemoveFilter(Double.valueOf(params.get(FIRST_DECILES_NUM)));
+        MinWeightsRemoveFilter minWeightsRemoveFilter = new MinWeightsRemoveFilter(Integer.valueOf(params.get(MIN_WEIGHTS_NUM)));
         MinNodeFilter minNodeFilter = new MinNodeFilter(Integer.valueOf(params.get(MIN_NODES)));
-        FiltersApplier filtersApplier = new FiltersApplier(firstDecileRemoveFilter, minNodeFilter);
+//        FiltersApplier filtersApplier = new FiltersApplier(firstDecileRemoveFilter, minNodeFilter);
+        FiltersApplier filtersApplier = new FiltersApplier(minWeightsRemoveFilter, minNodeFilter);
         newGraph = filtersApplier.execute(newGraph);
         DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(outputFile));
         for (Island island : newGraph.getIslands()) {
